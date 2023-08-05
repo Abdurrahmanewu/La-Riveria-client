88c5fb4d456f716ef774738944e81a58f83bbb70
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2023-08-05 23:41:10 +0600

    slider complete

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -7,28 +7,32 @@ import SliderItems from "./SliderItems";
 const Slider = () => {
   const sliderData = [
     {
-      img: img1,
+      image: img1,
       prev: 3,
       id: 1,
       next: 2,
     },
     {
-      img: img2,
+      image: img2,
       prev: 1,
       id: 2,
       next: 3,
     },
     {
-      img: img3,
-      prev: 1,
+      image: img3,
+      prev: 2,
       id: 3,
-      next: 2,
+      next: 1,
     },
   ];
   return (
     <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img src={img1} className="w-full" alt="" />
+      {sliderData.map((slide) => (
+        <SliderItems key={slide.id} slide={slide}></SliderItems>
+      ))}
+      {/* <SliderItems image={img1}></SliderItems> */}
+      {/* <div id="slide1" className="carousel-item relative w-full">
+        <img src={img1} className="w-full carousel-img" alt="" />
         <div className="absolute flex justify-end transform -translate-y-1/2 text-center left-32 top-1/4">
           <h1 className="text-5xl font-bold text-white">
             Welcome To La Riveria <br />
@@ -43,8 +47,8 @@ const Slider = () => {
             ❯
           </a>
         </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
+      </div> */}
+      {/* <div id="slide2" className="carousel-item relative w-full">
         <img src={img2} className="w-full" alt="" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide1" className="btn btn-circle">
@@ -65,10 +69,7 @@ const Slider = () => {
             ❯
           </a>
         </div>
-      </div>
-      {/* {sliderData.map((slide) => (
-        <SliderItems key={slide.id} slide={slide}></SliderItems>
-      ))} */}
+      </div> */}
     </div>
   );
 };
--- src/Components/Slider/SliderItems.jsx
+++ src/Components/Slider/SliderItems.jsx
@@ -1,41 +1,24 @@
 import React from "react";
-import "./SliderItems.css";
+import "./SliderStyle.css";
 
 const SliderItems = ({ slide }) => {
-  const { id, img, prev, next } = slide;
+  const { id, image, prev, next } = slide;
   return (
-    <div>
-      <div id={`slide${id}`} className="carousel-item relative w-full">
-        <div className="carousel-img">
-          <img src={img} className="w-full rounded-xl" alt="" />
-        </div>
-        <div className=" absolute  transform -translate-y-1/2 left-24 top-1/4">
-          <h1 className="text-6xl font-bold text-white">
-            Affordable <br />
-            Price for car <br />
-            Servicing
-          </h1>
-        </div>
-        <div className=" absolute  transform -translate-y-1/2 left-24 top-1/2">
-          <p className="text-xl text-white">
-            There are many variations of passages of available, but <br />
-            the majority have suffered alteration in some form
-          </p>
-        </div>
-        <div className=" absolute  transform -translate-y-1/2 left-24 top-3/4">
-          <button className="btn btn-warning mr-5">Discover More</button>
-          <button className="btn btn-outline btn-warning">
-            Latest Projects
-          </button>
-        </div>
-        <div className=" absolute flex transform -translate-y-1/2 right-5 bottom-0">
-          <a href={`#slide${prev}`} className="btn btn-circle mr-5">
-            ❮
-          </a>
-          <a href={`#slide${next}`} className="btn btn-circle ">
-            ❯
-          </a>
-        </div>
+    <div id={`slide${id}`} className="carousel-item relative w-full">
+      <img src={image} className="w-full carousel-img" alt="" />
+      <div className="absolute flex justify-end transform -translate-y-1/2 text-center left-32 top-1/4">
+        <h1 className="text-5xl font-bold text-white">
+          Welcome To La Riveria <br />
+          Resort & Park
+        </h1>
+      </div>
+      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href={`#slide${prev}`} className="btn btn-circle mr-5">
+          ❮
+        </a>
+        <a href={`#slide${next}`} className="btn btn-circle">
+          ❯
+        </a>
       </div>
     </div>
   );
